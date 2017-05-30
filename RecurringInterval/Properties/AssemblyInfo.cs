1423e210e765ea63f46a10607528af7113c4986b
dan <dan@danglass.com>
2017-05-29 17:24:27 -0700

    Portable class library was a disaster - moving back to .net4*

--- RecurringInterval/Properties/AssemblyInfo.cs
+++ RecurringInterval/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -7,14 +6,21 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RecurringInterval")]
-[assembly: AssemblyDescription("C# library for calculating intervals and the duration between days, weeks, fortnights, bimonthly, months, quarters and years")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RecurringInterval")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8e149d4b-d8d6-4414-b691-d6e79f668444")]
 
 // Version information for an assembly consists of the following four values:
 //
